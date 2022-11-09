8e004fe7858b2b47b9280f4ee9075ae73a2bc8e0
RundesBalli <GitHub@RundesBalli.com>
2022-11-09 13:30:28 +0100

    Send chatId as code block

--- bot.php
+++ bot.php
@@ -36,13 +36,13 @@ if(empty($response)) {
  * Checking if a group invitation was done.
  */
 if(isset($response['message']['my_chat_member'])) {
-  sendMessageToTelegram($response['message']['my_chat_member']['chat']['id'], $response['message']['my_chat_member']['chat']['id']);
+  sendMessageToTelegram('`'.$response['message']['my_chat_member']['chat']['id'].'`', $response['message']['my_chat_member']['chat']['id']);
   die();
 }
 
 /**
  * A private message was sent to the bot.
  */
-sendMessageToTelegram($response['message']['chat']['id'], $response['message']['chat']['id']);
+sendMessageToTelegram('`'.$response['message']['chat']['id'].'`', $response['message']['chat']['id']);
 die();
 ?>
